f3eb41afad5e4a0ae35856c51b0830cc9da77cd1
neeltechie <neeltechie@gmail.com>
2018-05-11 17:32:43 +0530

    Update WikiObject.py
    tada

--- SRC/WikiObject.py
+++ SRC/WikiObject.py
@@ -1,31 +1,65 @@
-from bs4 import BeautifulSoup as bs
-import requests
-import wikipedia
-import warnings
+from __future__ import division
+from numpy.random import random
+deuce_flag = 0  # A Global variable to indicate Deuce
 
 
-class WikiObject:
-    def __init__(self, string):
-        self.article_name = string
-        self.page_details = None
+def sim_round(points, prob_a):
+    """ sim_round simulates the round where program decides which player have won the game """
+    if random() < prob_a:
+        points['A'] = points['A'] + 1
+    else:
+        points['B'] = points['B'] + 1
+    return points
 
-    def check_validity(self):
-        try:
-            self.page_details = wikipedia.page(self.article_name)
-            if self.page_details.title != self.article_name:
-                warnings.warn("Given Article Name {0} could not be find. Using {1} instead".
-                              format(self.article_name, self.page_details.title))
-                return True
-        except wikipedia.exceptions.PageError as e:
-            print e
+
+def stopping_criteria(points):
+    """stopping_criteria indicates when a single game shall be stopped"""
+    deuce(points)
+    if deuce_flag == 0:
+        if max(points['A'], points['B']) == 11:
             return False
+    else:
+        if abs(points['A'] - points['B']) == 2:
+                return False
+        else:
+                return True
+    return True
 
-    def get_links(self):
-        if self.check_validity():
-            link_details = self.page_details.links
-            return link_details
+
+def deuce(points):
+    """when deuce happens deuce function raise a flag which will be used in stopping_criteria"""
+    global deuce_flag
+    if points['A'] == points['B'] and points['A'] == 10:
+        deuce_flag = 1
+
+
+def sim_game(target_score, prob_a):
+    """sim_game simulates one single game and then its final score matches with target score or not"""
+    global deuce_flag
+    deuce_flag = 0
+    continue_game = True
+    points = {'A': 0, 'B': 0}
+    deuce_flag = 0
+    while continue_game:
+        points = sim_round(points, prob_a)
+        continue_game = stopping_criteria(points)
+    if points['A'] == target_score['A'] and points['B'] == target_score['B']:
+        return 1
+    else:
+        return 0
+
+
+def montecarlo(target_score, prob_a, num_sim):
+    """Montecarlo simulates the Monte Carlo method.
+    Its input are target score, winning prob of A and number of sample"""
+    success = 0
+    for i in xrange(num_sim):
+        success = success + sim_game(target_score, prob_a)
+    print success
+    print success / num_sim
 
 
 if __name__ == '__main__':
-    test_object = WikiObject('akbarh')
-    test_object.get_links()
+    target_score = {'A': 11, 'B': 4}
+    prob_A = 0.5
+    montecarlo(target_score, prob_A, 100000)
